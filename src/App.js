13d4a660a72e6d1efca63b36e6a006edd822194c
Andrew Garvin <me@andrewgarv.in>
2019-11-28 13:05:59 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/main.scss';
+
+function App() {
+  return (
+    <main className="App">
+      <h1>hello world</h1>
+    </main>
+  );
+}
+
+export default App;
